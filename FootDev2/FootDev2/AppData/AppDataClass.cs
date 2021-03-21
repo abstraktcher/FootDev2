3ace8daf86b4a71fd743f71a08cfaa839ec6b065
wfbleezy <Vasilii.bavkaev@gmail.com>
2021-03-21 21:56:55 +0300

    I made connection to DataBase, and few forms.

---
+++ FootDev2/FootDev2/AppData/AppDataClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootDev2.AppData
+{
+    class AppDataClass
+    {
+       public static FootDevEntities context { get; } = new FootDevEntities();
+    }
+}
---
+++ FootDev2/FootDev2/Helper1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FootDev2
+{
+    /// <summary>
+    /// Логика взаимодействия для Helper1.xaml
+    /// </summary>
+    public partial class Helper1 : Window
+    {
+        public Helper1()
+        {
+            InitializeComponent();
+        }
+    }
+}
